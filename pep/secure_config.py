32b97b4ad1c0cc86f1aad920f0ad6b348bc04e2c
jjin <jiankun.jin@gmail.com>
2024-05-28 12:30:24 +0800

    update folder structure
    add pep
    
    update secret

---
+++ pep/secure_config.py
@@ -0,0 +1,42 @@
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import base64
+import yaml
+from getpass import getpass
+
+def gen_fernet_key(pwd:str) -> bytes:
+    password = pwd.encode()
+    salt = '+6u934mwrWZ7x+7UvMYbSA=='
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=base64.b64decode(salt),
+        iterations=480000,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    return key
+
+def encrypt(str, passwd):
+    f = Fernet(gen_fernet_key(passwd))
+    return f.encrypt(str.encode()).decode()
+
+def decrypt(str, passwd):
+    f = Fernet(gen_fernet_key(passwd))
+    return f.decrypt(str.encode()).decode()
+
+# read yaml file 'config.yaml' into a map
+def read_config():
+    config = None
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+    password = getpass()
+    if ('tencent.ocr' in config):
+        if ('secret_id' in config['tencent.ocr']):
+            config['tencent.ocr']['secret_id'] = decrypt(config['tencent.ocr']['secret_id'], password)
+        if ('secret_key' in config['tencent.ocr']):
+            config['tencent.ocr']['secret_key'] = decrypt(config['tencent.ocr']['secret_key'], password)
+    return config['tencent.ocr']
+
+if __name__ == '__main__':
+    read_config()
